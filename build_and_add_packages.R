c838c22961d9ecc901e5afecd4720189ff5eeb53
Mauro Lepore <maurolepore@gmail.com>
2019-02-12 11:34:37 -0500

    Rewrite source PACKAGES

--- build_and_add_packages.R
+++ build_and_add_packages.R
@@ -1,5 +1,37 @@
 # Functions ----
 
+write_src_PACKAGES <- function(dir = path_src()) {
+  tools::write_PACKAGES(dir = dir, type = "source")
+}
+
+write_mac_PACKAGES <- function(dir = path_mac()) {
+  tools::write_PACKAGES(dir = dir, type = "mac.binary")
+}
+
+write_win_PACKAGES <- function(dir = path_win()) {
+  tools::write_PACKAGES(dir = dir, type = "win.binary")
+}
+
+this_r_version <- function() {
+  paste0(R.version$major, ".", pull_minor(), collapse = ".")
+}
+
+pull_minor <- function() {
+  strsplit(R.version$minor, ".", fixed = TRUE)[[1]][[1]]
+}
+
+path_src <- function() {
+  here::here("src", "contrib")
+}
+path_win <- function() {
+  here::here("bin", "windows", "contrib", this_r_version())
+}
+path_mac <- function(mac_version = "el-capitan") {
+  here::here("bin", "macosx", "contrib", mac_version, this_r_version())
+}
+
+
+
 build_and_add_source <- function(path, repodir = ".") {
   drat::insertPackage(devtools::build(path, binary = FALSE), repodir = repodir)
 }

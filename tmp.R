c6c89df18879a4b82b008221f850d485b46a5f79
Mauro Lepore <maurolepore@gmail.com>
2019-05-06 17:43:49 -0400

    Add win-binaries to R 3.6

--- tmp.R
+++ tmp.R
@@ -1,3 +1,17 @@
+# Add support for new minor (or greater) version of R ---------------------
+
+# Download the .zip of each realease
+# Unzip in Downloads and remove the .zip
+
+path <- fs::dir_ls("~/Downloads/", regexp = "fgeo")
+path <- fs::path(path, fs::path_file(path))
+path %>%
+  purrr::walk(
+    ~ drat::insertPackage(devtools::build(.x, binary = TRUE), repodir = ".")
+  )
+
+# Adding packages ---------------------------------------------------------
+
 build_and_add_source("../fgeo.krig")
 
 build_and_add_binary("../fgeo.krig")
